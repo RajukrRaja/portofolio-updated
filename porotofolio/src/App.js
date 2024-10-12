b8ebdf0b6c420ad980d7e6d0ebb80c5bfe68c4d8
Raju Kumar Raja <rajukumar.191813@gmail.com>
2024-10-12 15:59:56 +0530

    enhance about page

--- porotofolio/src/App.js
+++ porotofolio/src/App.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
 import Services from './Components/Services/Services';
 import Contact from './Components/Pages/Contact';
-import Myproject from './Components/Myproject/Myproject'
+import Myproject from './Components/Myproject/Myproject';
 import Footer from './Components/Footer/Footer';
 import Info from './Components/Info/Info';
 import Skill from './Components/skill/skill';
-
+// Optional: Create a NotFound component
 
 function App() {
   return (
     <div>
       <Navbar />
-
+      {/* Conditional Rendering for Home */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/myproject" element={<Myproject/>} />
+        <Route path="/myproject" element={<Myproject />} />
         <Route path="/contact" element={<Contact />} />
+        {/* Redirect to home if route not found */}
+        
       </Routes>
-      <Info/>
- 
-   <Skill/>
-
+      
+      {/* Components that should be displayed on all pages */}
+      <Info />
+      <Skill />
       <Myproject/>
-      <About/>
-
-      <Services/>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
